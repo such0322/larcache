6a98f78884631707d3c16acc04c578c76329ba0f
such0322 <such0322@hotmail.com>
2016-11-22 16:58:19 +0800

    test

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -18,6 +18,7 @@ class IndexController extends Controller
      */
     public function index()
     {
+		echo "this is test";
         //
 //        LPUSH list1 "foo"
 //        for($i=10;$i<20;$i++){
